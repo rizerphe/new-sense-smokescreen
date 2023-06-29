69ea998ffa62db41e97cdb4d9d7b33004722b228
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 10:37:53 +0300

    Translate into Ukrainian

--- src/bias.tsx
+++ src/bias.tsx
@@ -4,7 +4,7 @@ import HeadingBlock from "./HeadingBlock";
 export default function Bias() {
   return (
     <div className="w-full p-4 flex flex-row-reverse flex-wrap items-center gap-4">
-      <HeadingBlock intro="Get rid of" title="the bias." />
+      <HeadingBlock intro="Позбудься" title="упереджень." />
       <div className="w-full flex flex-column p-4">
         <div className="w-full flex flex-col items-left gap-4 p-4 border-2 rounded">
           <NewsBlock />
--- src/news.tsx
+++ src/news.tsx
@@ -4,7 +4,7 @@ import HeadingBlock from "./HeadingBlock";
 export default function News() {
   return (
     <div className="w-full p-4 flex flex-row flex-wrap items-center gap-4">
-      <HeadingBlock intro="Be less" title="overwhelmed." />
+      <HeadingBlock intro="Будь менш" title="перевантаженим." />
       <div className="w-full flex flex-column p-4">
         <div className="w-full flex flex-col items-left gap-4 p-4 border-2 rounded">
           <NewsBlock />
