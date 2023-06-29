05ab70af6d6d517021e9f26b9faa666d8bf5c52f
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 18:48:55 +0300

    Change title text

--- src/components/bias.tsx
+++ src/components/bias.tsx
@@ -4,7 +4,7 @@ import HeadingBlock from "./HeadingBlock";
 export default function Bias() {
   return (
     <div className="biases w-full p-4 flex flex-col md:flex-row-reverse flex-wrap items-center gap-4">
-      <HeadingBlock intro="Протистій" title="упередженням." />
+      <HeadingBlock intro="Не будь" title="упередженим." />
       <div className="flex flex-1 flex-column min-w-[50vw] p-4">
         <div className="flex flex-col items-left border-2 rounded">
           <NewsBlock collapsed />
