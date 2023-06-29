8091e61b80517533cf6abfa599edd1202540e6bf
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-30 01:38:52 +0300

    Link to the database

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,16 +1,52 @@
+"use client";
+import { useState } from "react";
+import { MdNavigateNext } from "react-icons/md";
+
 export default function BetaVersionDownload() {
+  const [email, setEmail] = useState("");
+  const [useCase, setUseCase] = useState("");
+
+  const joinWaitlist = (email: string, useCase: string) => {
+    await fetch("/joinWaitlist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, useCase }),
+    });
+  };
+
   return (
     <div className="flex flex-col gap-8 items-center justify-center h-full">
       <div className="text-4xl font-bold">Ми в рамках бета-тестування</div>
-      <div className="flex flex-row items-center justify-center">
+      <div className="text-sm w-1/2">
+        Ми вже майже готові до публічного запуску, а поки що ви можете
+        приєднатися до списку очікування - ми повідомимо вас, як тільки ви
+        отримаєте доступ до нашого сервісу.
+      </div>
+      <div className="flex flex-col gap-0 items-center justify-center">
         <input
-          className="w-1/2 p-2 m-2 border border-gray-400 rounded-md"
-          type="email"
-          placeholder="Введіть свій email"
+          className="w-full p-2 m-2 border border-gray-400 rounded-md"
+          type="text"
+          placeholder="Як ви плануєте використовувати наш сервіс?"
+          value={useCase}
+          onChange={(e) => setUseCase(e.target.value)}
         />
-        <button className="w-1/2 p-2 m-2 border border-gray-400 whitespace-nowrap min-w-fit hover:bg-gray-100 rounded-md">
-          Приєднатися до списку очікування
-        </button>
+        <div className="w-full m-2 gap-4 flex flex-row items-center justify-center">
+          <input
+            className="w-1/2 p-2 border border-gray-400 rounded-md"
+            type="email"
+            placeholder="Ваш email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            className="flex flex-row items-center gap-1 w-1/2 p-2 border border-gray-400 whitespace-nowrap min-w-fit hover:bg-gray-100 rounded-md"
+            onClick={joinWaitlist}
+          >
+            Приєднатися до списку очікування <MdNavigateNext />
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/app/joinWaitlist/route.ts
@@ -0,0 +1,53 @@
+import admin from "firebase-admin";
+import { NextResponse } from "next/server";
+
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY || ""),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
+const app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as any),
+  databaseURL:
+    "https://newsense-2c524-default-rtdb.europe-west1.firebasedatabase.app",
+});
+const database = admin.database(app);
+
+function validateEmail(email: string) {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+export async function POST(request: Request) {
+  try {
+    const { email, reason }: { email: string; reason: string } =
+      await request.json();
+    if (!reason || !email || !validateEmail(email)) {
+      return NextResponse.json(
+        { error: "Invalid email or reason" },
+        { status: 400 }
+      );
+    }
+    const existing = await database
+      .ref("waitlist")
+      .orderByChild("email")
+      .equalTo(email)
+      .once("value");
+    if (existing.exists()) {
+      return NextResponse.json(
+        { error: "Email already on waitlist" },
+        { status: 400 }
+      );
+    }
+    const record = {
+      email: email,
+      reason: reason,
+      timestamp: Date.now(),
+    };
+    const recordRef = await database.ref("waitlist").push(record);
+    return NextResponse.json({ id: recordRef.key });
+  } catch (error) {
+    return NextResponse.json({}, { status: 500 });
+  }
+}
