cd468bdc3ba515e5b9ad7a4acf1e5ee7870df06d
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 18:45:59 +0300

    Removed download from the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,9 +20,6 @@ export default function Navbar() {
       <a href="/about" className="text-gray-400 m-2 ml-auto text-lg font-bold">
         Про нас
       </a>
-      <a href="/download" className="text-gray-400 m-2 text-lg font-bold">
-        Завантажити
-      </a>
     </div>
   );
 }
