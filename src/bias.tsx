c7a044843e7483769aecb26c297dc3f0cf1aeb1b
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 10:32:36 +0300

    Change flex order

--- src/bias.tsx
+++ src/bias.tsx
@@ -3,13 +3,13 @@ import HeadingBlock from "./HeadingBlock";
 
 export default function Bias() {
   return (
-    <div className="w-full p-4 flex flex-row flex-wrap items-center gap-4">
+    <div className="w-full p-4 flex flex-row-reverse flex-wrap items-center gap-4">
+      <HeadingBlock intro="Get rid of" title="the bias." />
       <div className="w-full flex flex-column p-4">
         <div className="w-full flex flex-col items-left gap-4 p-4 border-2 rounded">
           <NewsBlock />
         </div>
       </div>
-      <HeadingBlock intro="Get rid of" title="the bias." />
     </div>
   );
 }
