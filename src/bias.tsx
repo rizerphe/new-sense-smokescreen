ef62f59716fc479455301c7e67674a4e6e14383b
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 11:16:45 +0300

    Add loading animation

--- src/bias.tsx
+++ src/bias.tsx
@@ -3,7 +3,7 @@ import HeadingBlock from "./HeadingBlock";
 
 export default function Bias() {
   return (
-    <div className="w-full p-4 flex flex-col md:flex-row-reverse flex-wrap items-center gap-4">
+    <div className="biases w-full p-4 flex flex-col md:flex-row-reverse flex-wrap items-center gap-4">
       <HeadingBlock intro="Позбудься" title="упереджень." />
       <div className="flex flex-1 flex-column min-w-[50vw] p-4">
         <div className="flex flex-col items-left gap-4 p-4 border-2 rounded">
