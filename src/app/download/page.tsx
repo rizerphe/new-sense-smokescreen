cc9f7c9de135eeb18c80d9f53626d68faefca6f2
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 18:41:23 +0300

    Add an about me

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,15 +1,15 @@
 export default function BetaVersionDownload() {
   return (
     <div className="flex flex-col gap-8 items-center justify-center h-full">
-      <div className="text-4xl font-bold">We are in a limited beta</div>
+      <div className="text-4xl font-bold">Ми в рамках бета-тестування</div>
       <div className="flex flex-row items-center justify-center">
         <input
           className="w-1/2 p-2 m-2 border border-gray-400 rounded-md"
           type="email"
-          placeholder="Enter your email"
+          placeholder="Введіть свій email"
         />
-        <button className="w-1/2 p-2 m-2 border border-gray-400 hover:bg-gray-100 rounded-md">
-          Join the waitlist
+        <button className="w-1/2 p-2 m-2 border border-gray-400 whitespace-nowrap min-w-fit hover:bg-gray-100 rounded-md">
+          Приєднатися до списку очікування
         </button>
       </div>
     </div>
