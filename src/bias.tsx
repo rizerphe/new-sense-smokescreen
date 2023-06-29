c88e7ce07988e0af22920dc2b9e281daee2ea64b
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-29 10:29:13 +0300

    Add mobile friendly warp

--- src/bias.tsx
+++ src/bias.tsx
@@ -3,7 +3,7 @@ import HeadingBlock from "./HeadingBlock";
 
 export default function Bias() {
   return (
-    <div className="w-full p-4 flex flex-row items-center gap-4">
+    <div className="w-full p-4 flex flex-row flex-wrap items-center gap-4">
       <div className="w-full flex flex-column p-4">
         <div className="w-full flex flex-col items-left gap-4 p-4 border-2 rounded">
           <NewsBlock />
